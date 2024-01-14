77b2c0c9a449a96e2ce40e9ca52cb511b64948f6
ishanluhani <ishanluhani@gmail.com>
2024-01-14 20:59:50 +0530

    test1

--- create analysis from manifest.py
+++ create analysis from manifest.py
@@ -2,11 +2,21 @@ from PyPDF2 import PdfReader
 from tkinter.filedialog import askdirectory
 import pandas as pd
 import os
+import json
 
 
 # Defining variable that will be used Later on
 sku_size_quantity_dict = {}
 colors = ['Multicolor', 'Teal', 'Grey', 'Navy', 'Blue', 'White', 'Beige', 'Maroon', 'Orange', 'Brown', 'Black', 'Yellow', 'Red', 'Pink', 'NA', 'Green', 'OVERSIZE']
+mapping_of_groups = {1: 'GREY-CHECK', 2: 'ROYAL-BLUE-CHECK', 3: 'WHITE-CHECK', 4: 'BROWN-CHECK', 5: 'NAVY-BLUE-CHECK',
+                     6: 'RED-CHECK', 7: 'NAVY-NARROW-STRIPES', 8: 'WHITE-NARROW-STRIPES', 9: 'DARK-GREY-NARROW-STRIPES',
+                     10: 'OVERSIZE-FINE-PRINT-BLACK', 11: 'OVERSIZE-FINE-PRINT-SEA-GREEN',
+                     12: 'OVERSIZE-FINE-PRINT-BLUE', 13: 'OVERSIZE-FINE-PRINT-MAROON', 14: 'DUAL-LINE-CHECK-BEIGE',
+                     15: "DUAL-LINE-CHECK-NAVY", 16: "DUAL-LINE-CHECK-MAROON", 17: 'ROYAL-BLUE-PLAIN',
+                     18: 'PISTA-PLAIN', 19: "NAVY-PLAIN", 20: 'CEMENT-PLAIN', 21: "RUST-PLAIN", 22: "PINK-PLAIN",
+                     23: "GREY-DARK-PLAIN-PRODUCT", 24: 'MAROON-PLAIN', 25: "BOTTLE-GREEN-PLAIN", 26: "SKY-PLAIN",
+                     27: "BLACK-PLAIN", 28: 'WHITE-PLAIN', 29: "NAVY-BROTHERS-YELLOW", 30: "NAVY-BROTHERS-GREY",
+                     31: "NAVY-BROTHERS-RED"}
 folder_path = askdirectory()
 
 for file_name in os.listdir(folder_path):
@@ -52,7 +62,30 @@ for file_name in os.listdir(folder_path):
 
 
 sku_size_quantity_list = [[i[0], i[1], sku_size_quantity_dict[i]] for i in sku_size_quantity_dict]
-exporting_dataframe = pd.DataFrame(sku_size_quantity_list)
+data = json.load(open('mapping of skus.json'))
+
+for i in data:
+    data[i] = str(data[i])
+
+for i in range(len(sku_size_quantity_list)):
+    x = sku_size_quantity_list[i][0]
+    if data.get(x):
+        sku_size_quantity_list[i][0] = mapping_of_groups[int(data[sku_size_quantity_list[i][0]])]
+    else:
+        sku_size_quantity_list[i][0] = sku_size_quantity_list[i][0]
+
+sku_size_quantity_list_after_collapse = {}
+for i in sku_size_quantity_list:
+    set_ = tuple(i[:2])
+    if sku_size_quantity_list_after_collapse.get(set_):
+        print(sku_size_quantity_list_after_collapse[set_], i)
+        sku_size_quantity_list_after_collapse[set_] = sku_size_quantity_list_after_collapse[set_][:2] + [sku_size_quantity_list_after_collapse[set_][2] + i[2]]
+    else:
+        sku_size_quantity_list_after_collapse[set_] = i
+
+sku_size_quantity_list_after_collapse = sku_size_quantity_list_after_collapse.values()
+
+exporting_dataframe = pd.DataFrame(sku_size_quantity_list_after_collapse)
 exporting_dataframe.columns = ['SKU', 'SIZE', 'QUANTITY']
 sizes = ['S', 'M', 'L', 'XL', 'XXL', 'XXXL', '4XL']
 
@@ -63,8 +96,7 @@ for size in sizes:
     exporting_dataframe = exporting_dataframe._append({'SKU': 'TOTAL', 'SIZE': size, 'QUANTITY': exporting_dataframe[exporting_dataframe['SIZE'] == size]['QUANTITY'].sum()}, ignore_index=True)
 
 exporting_dataframe['SIZE'] = exporting_dataframe['SIZE'].astype('category')
-exporting_dataframe['SIZE'] = exporting_dataframe['SIZE'].cat.reorder_categories(new_categories=sizes,
-                                                                                                ordered=True)
+exporting_dataframe['SIZE'] = exporting_dataframe['SIZE'].cat.reorder_categories(new_categories=sizes, ordered=True)
 
 exporting_dataframe = pd.pivot_table(exporting_dataframe, index='SKU', columns='SIZE', values='QUANTITY')
 exporting_dataframe = exporting_dataframe.fillna(0)
@@ -72,6 +104,5 @@ exporting_dataframe = exporting_dataframe.fillna(0)
 exporting_dataframe.loc['TOTAL ', :] = exporting_dataframe.loc['TOTAL', :].to_dict()
 exporting_dataframe.drop('TOTAL', inplace=True)
 
-print(exporting_dataframe)
 exporting_dataframe.to_excel('output.xlsx')
 # exporting_dataframe.to_excel('output.xlsx', index=False)
